c99cdad447c444a24d9904cf8dcab958b224ad4c
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-08-27 16:38:28 -0700

    grid columns and num of images reacts to screen size

--- src/components/GalleryWave.js
+++ src/components/GalleryWave.js
@@ -21,17 +21,31 @@ const extendArrayToLength = (array, desiredLength) => {
 };
 
 export default function GalleryWave({ images_arr, viewportWidth }) {
-    const desiredLength = 24; // adjust this as needed
-    const numOfImagesPerRow = desiredLength / 4;
-
+    let numOfImagesPerRow;
+    
+    if (viewportWidth <= 500){
+        numOfImagesPerRow = 6;
+    } else if (viewportWidth > 500 && viewportWidth <= 900){
+        numOfImagesPerRow = 7;
+    } else if( viewportWidth > 900 && viewportWidth <= 1080){
+        numOfImagesPerRow = 8;
+    } else if ( viewportWidth > 1080 && viewportWidth <= 1450){
+        numOfImagesPerRow = 9;
+    } else if (viewportWidth > 1450 && viewportWidth <= 2560){
+        numOfImagesPerRow = 12;
+    } else {
+        numOfImagesPerRow = viewportWidth / 250;
+    }
+    const desiredLength = numOfImagesPerRow * 4; // adjust this as needed
+    
     const extendedImagesArr = extendArrayToLength(images_arr, desiredLength);
-// i need to break this down and make sure I understand it
+    // i need to break this down and make sure I understand it
     const extendedImagesArrWithAnimationIDs = extendedImagesArr.map((imgOBJ, index) => {
         const rowNumber = Math.floor(index / numOfImagesPerRow);
         const animation_id = (index % numOfImagesPerRow) + 1 + rowNumber;
         return { ...imgOBJ, animation_id };
     });
-
+    
     const GalleryWaveDisplay = extendedImagesArrWithAnimationIDs.map((imgObj, index) => {
         return (
             <ImgWrapper key={`${imgObj.id}-${index}`}>
@@ -44,9 +58,10 @@ export default function GalleryWave({ images_arr, viewportWidth }) {
 
     return (
         <GalleryWaveContainer>
-            <GalleryGrid>
+            <GalleryGrid numofimagesperrow={numOfImagesPerRow}>
                 {GalleryWaveDisplay}
             </GalleryGrid>
+                {numOfImagesPerRow}
         </GalleryWaveContainer>
     );
 }
--- src/styles/GalleryWaveStyles.js
+++ src/styles/GalleryWaveStyles.js
@@ -6,39 +6,8 @@ export const GalleryWaveContainer = styled.div`
 
 export const GalleryGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(60px ,1fr));
+    grid-template-columns: repeat(${(props) => props.numofimagesperrow}, 1fr);
     gap: 2px;
-    
-    @media (min-width: 430px ) {
-    grid-template-columns: repeat(auto-fit, minmax(65px ,1fr));
-    }
-
-    @media (min-width: 465px ) {
-    grid-template-columns: repeat(auto-fit, minmax(70px ,1fr));
-    }
-
-    @media (min-width: 500px ) {
-    grid-template-columns: repeat(auto-fit, minmax(75px ,1fr));
-    }
-
-    @media (min-width: 535px ) {
-    grid-template-columns: repeat(auto-fit, minmax(80px ,1fr));
-    }
-
-    @media (min-width: 570px ) {
-    grid-template-columns: repeat(auto-fit, minmax(85px ,1fr));
-    }
-
-    @media (min-width: 605px ) {
-    grid-template-columns: repeat(auto-fit, minmax(90px ,1fr));
-    }
-
-    @media (min-width: 730px ) {
-    grid-template-columns: repeat(auto-fit, minmax(95px ,1fr));
-    }
-    @media (min-width: 1760px ) {
-    grid-template-columns: repeat(auto-fit, minmax(150px ,1fr));
-    }
 `;
 
 export const ImgWrapper = styled.div`
