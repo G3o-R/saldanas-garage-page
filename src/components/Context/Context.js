4f1778b844c4444526072a948e07f1a7cf17dcc1
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-08-29 12:46:30 -0700

    forgot to change fetch route

--- src/components/Context/Context.js
+++ src/components/Context/Context.js
@@ -7,8 +7,8 @@ function ContextProvider({ children }) {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        // fetch("https://saldanas-garage-page-json-server.onrender.com/page_components")
-        fetch("http://localhost:3001/page_components")
+        fetch("https://saldanas-garage-page-json-server.onrender.com/page_components")
+        // fetch("http://localhost:3001/page_components")
             .then((res) => res.json())
             .then((data) => {
                 setPageComponents(data);
