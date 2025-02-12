ad08f2e51fb6f91a700bf57ccc3dc1b532efbf4b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2025-02-11 20:41:49 -0700

    don't know what's causing overflow just adding overflow hidden to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,8 +10,8 @@ import CTA from "../sections/CTA";
 export default function Home(){
     return(
         <HomePage>
-            {/* <HeroVideo /> */}
-            {/* <Content /> */}
+            <HeroVideo />
+            <Content />
             <CTA />
             <Footer />
         </HomePage>
